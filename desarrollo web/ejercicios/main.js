787308b9c571e51d46a944028c4a6c2de5fcfc7f
fernandres <fdoquezadapuno@gmail.com>
2022-02-03 16:57:59 -0300

    creación de carpeta de ejercicios
    carpeta para ejercitar

---
+++ desarrollo web/ejercicios/main.js
@@ -0,0 +1,13 @@
+var formulario =document.getElementById("nombreFormulario");
+
+
+let nombre_usuario = formulario.value;
+
+function valorRequerido(input) {
+    if (input == "") { 
+        alert("el campo esta vacio")
+} else {
+    alert (input);
+    }
+ }
+
